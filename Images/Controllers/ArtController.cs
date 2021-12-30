a05c467a2561e39ef644cac8f59ee26e9784cb08
LeeBate <harukaxd02@gmail.com>
2021-12-30 23:53:59 +0700

    login

---
+++ Images/Controllers/ArtController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Images.Controllers
+{
+    public class ArtController : Controller
+    {
+        // GET: Art
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult art()
+        {
+            return View();
+        }
+    }
+}
